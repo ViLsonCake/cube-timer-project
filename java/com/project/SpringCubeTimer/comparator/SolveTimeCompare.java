160dd7a9ce12061b2ce91169f9d08f4236654943
ViLsonCake <114938803+ViLsonCake@users.noreply.github.com>
2023-05-01 02:01:15 +0200

    fix compare bug

--- java/com/project/SpringCubeTimer/comparator/SolveTimeCompare.java
+++ java/com/project/SpringCubeTimer/comparator/SolveTimeCompare.java
@@ -11,6 +11,11 @@ public class SolveTimeCompare implements Comparator<SolveEntity> {
 
     @Override
     public int compare(SolveEntity o1, SolveEntity o2) {
+        if (o1.getTime().equalsIgnoreCase("DNF"))
+            return 1;
+        else if (o2.getTime().equalsIgnoreCase("DNF"))
+            return -1;
+
         return Integer.compare(TimerService.toSeconds(o1.getTime()), TimerService.toSeconds(o2.getTime()));
     }
 }
