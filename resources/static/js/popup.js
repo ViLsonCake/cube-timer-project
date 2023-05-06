f994ea846897d06308534e178691881139416aae
ViLsonCake <114938803+ViLsonCake@users.noreply.github.com>
2023-05-06 16:01:50 +0200

    add event listener to settings

--- resources/static/js/popup.js
+++ resources/static/js/popup.js
@@ -1,17 +1,40 @@
 const modalElement = document.getElementById('modal');
 const overlayElement = document.getElementById('overlay');
+const popupSettingsElements = document.querySelectorAll('slider.round');
 
 // Close popup
 overlayElement.addEventListener('click', () => {
     closeModal()
 });
 
+function closeModal() {
+    modalElement.classList.remove('active')
+    overlayElement.classList.remove('active')
+}
+
 function openModal() {
     modalElement.classList.add('active')
     overlayElement.classList.add('active')
 }
 
-function closeModal() {
-    modalElement.classList.remove('active')
-    overlayElement.classList.remove('active')
+function setDateExpires(days) {
+    let date = new Date;
+    date.setDate(date.getDate() + days)
+    return date
 }
+
+// Set property values
+popupSettingsElements.forEach(slider => {
+    slider.addEventListener('click', () => {
+        // Get element id
+        let sliderId = slider.id
+
+        if (slider.checked) {
+            // Add cookie
+            document.cookie = `${sliderId}=true; expires=${setDateExpires(365)}`
+        } else {
+            // Delete cookie
+            document.cookie = `${sliderId}=false; expires=-1`            
+        }
+    });
+});
